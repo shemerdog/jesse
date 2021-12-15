20558abbbfe299414312d8d620b08d3adcb04d76
Yehuda Gerstle <yehudag@d-fendsolutions.com>
2021-12-15 13:05:46 +0200

    feat(client): Add multilang support in i18n

--- client/public/js/i18n.js
+++ client/public/js/i18n.js
@@ -1,23 +1,36 @@
 const hasOwnProperty = {}.hasOwnProperty;
 
 const texts = {
-  "meals.breakfast.title": "breakfast",
-  "meals.lunch.title": "lunch",
+  en: {
+    "meals.breakfast.title": "breakfast",
+    "meals.lunch.title": "lunch",
 
-  "dishes.main.title": "main",
+    "dishes.main.title": "main",
 
-  "dishes.options.hotdog": "hotdog",
-  "dishes.options.burger": "burger",
-  "dishes.options.pita": "pita",
-  "dishes.options.bread-roll": "bread roll",
+    "dishes.options.hotdog": "hotdog",
+    "dishes.options.burger": "burger",
+    "dishes.options.pita": "pita",
+    "dishes.options.bread-roll": "bread roll",
+  },
+  he: {
+    "meals.breakfast.title": "ארוחת בוקר",
+    "meals.lunch.title": "צהרים",
+
+    "dishes.main.title": "עיקרית",
+
+    "dishes.options.hotdog": "נקניקיות",
+    "dishes.options.burger": "המבורגר",
+    "dishes.options.pita": "פיתה",
+    "dishes.options.bread-roll": "לחמנייה",
+  },
 };
 
 /**
  * @param {keyof texts} k
  * @returns {string}
  */
-export default function (k) {
-  if (hasOwnProperty.call(texts, k)) {
+export default function (k, lng = "en") {
+  if (hasOwnProperty.call(texts[lng], k)) {
     return texts[k];
   }
   return "";
